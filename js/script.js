2cd90d8c24e3f017bd3408844aee1ab7bb0c9b6a
Ben <bahalperin@gmail.com>
2014-10-09 23:10:30 -0400

    Updated portfolio styling

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
+var swap_image = function(image, original, replacement) {
+  $(image).hover(function() {
+    $(this).attr("src", replacement)
+  },
+  function() {
+    $(this).attr("src", original)
+  }
+)};
+
 $(document).ready(function() {
-  $('img.title-logo').hover(
-    function(){
-      $(this).attr('src','images/ninja.jpg')
-    },
-    function(){
-      $(this).attr('src','//placepuppy.it/100/100')
-    }
-)
+  swap_image("img.title-img","images/profile_pic.jpg","images/ninja.jpg");
 });
